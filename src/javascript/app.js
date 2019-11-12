db0b57a76c7f0e95a251740e52f7536e63740c97
mesa123123 <bowmanpete123@gmail.com>
2019-11-12 23:33:43 +1300

    First Commit

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -5,52 +5,15 @@ const mainDiv = document.querySelector("#main")
 window.addEventListener('load', () => {
     componentMounter(mainDiv)
 })
-
+// TODO: create a module bundler
+// TODO: create a templating engine
+  //CONCEPT: Tagged Template Literals - Vanilla Template Day 2
+  /* Apparently you can chuck whatever arguements you want in the func for want after the func`blah ${blah}` */
+  //CONCEPT: Strings.reduce function -- Vanilla Tample Day2 - How does it work?
+//First I will work through the template engine part of the front end framework builder course from mfrachet
 //TODO: create a router
 
 //TODO: create state management... somehow??
 console.log("end of file")
-//TODO: create an HTML builder
-const editor = grapesjs.init({
-  // Indicate where to init the editor. You can also pass an HTMLElement
-  container: '#gjs',
-  // Get the content for the canvas directly from the element
-  // As an alternative we could use: `components: '<h1>Hello World Component!</h1>'`,
-  fromElement: true,
-  // Size of the editor
-  height: '300px',
-  width: 'auto',
-  // Disable the storage manager for the moment
-  storageManager: false,
-  // Avoid any default panel
-  panels: { defaults: [] },
-  blockManager: {
-    appendTo: '#blocks',
-    blocks: [
-      {
-        id: 'section', // id is mandatory
-        label: '<b>Section</b>', // You can use HTML/SVG inside labels
-        attributes: { class:'gjs-block-section' },
-        content: `<section>
-          <h1>This is a simple title</h1>
-          <div>This is just a Lorem text: Lorem ipsum dolor sit amet</div>
-        </section>`,
-      }, {
-        id: 'text',
-        label: 'Text',
-        content: '<div data-gjs-type="text">Insert your text here</div>',
-      }, {
-        id: 'image',
-        label: 'Image',
-        // Select the component once it's dropped
-        select: true,
-        // You can pass components as a JSON instead of a simple HTML string,
-        // in this case we also use a defined component type `image`
-        content: { type: 'image' },
-        // This triggers `active` event on dropped components and the `image`
-        // reacts by opening the AssetManager
-        activate: true,
-      }
-    ]
-  }
-});
+//TODO: create a WYSIWYG editor and/or page builder
+//TODO: create a static site generator - and/or CMS?

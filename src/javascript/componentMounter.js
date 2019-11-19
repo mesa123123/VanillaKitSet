89faa43803485b198955f95610fae0a306a5a3bf
mesa123123 <bowmanpete123@gmail.com>
2019-11-20 07:33:50 +1300

    day6

--- src/javascript/componentMounter.js
+++ src/javascript/componentMounter.js
@@ -2,6 +2,8 @@
 const mountComponent = (domElement, componentToMount) => { 
     //The place to mount the target
     const mountTarget = document.querySelector(domElement)
+    //POSBUG create a custom component to attach to the DOM
+    customElements.define(componentToMount.type, createElement(componentToMount.template), {extends: componentToMount.childType})
     //Then attach the component to the target
     mountTarget.appendChild(componentToMount)
 }
@@ -9,3 +11,6 @@ const mountComponent = (domElement, componentToMount) => {
 export default mountComponent
 
 //CONCEPT: Vanilla Router Day 3 The create frontend framework tutorial shows a new element being created and then new text node being created, all out of the web component itself (the web component having both a type and a template and instead of passing in the mainDiv the name of the mainDiv is given and then queried from the page instead)
+//COULDI: day6 because the customElements.define method takes a constructor it could take a custom method that outputs a class method?
+//HOWDOI: day6 if I am to create custom components at the mounter in order to have custom named tags at the DOM I've got to somehow create new HTMLElement objects off of the templates in the component classes I've made in the component files
+//HOWDOI: day5 Maybe I just use a function in the customelements.define here???
--- src/javascript/components/SecondComponent.js
+++ src/javascript/components/SecondComponent.js
@@ -12,10 +12,7 @@ const p = createElement("p");
 export const User = ({firstName, lastName}) => {
  p`Hello ${firstName} ${lastName}`
 }
-//QUESTION: day5 why does he use {} with the ({}) in the USER arguments?
-//CONCEPT: day5 What this approach is missing is being able to use the custom tag name within the element
-//My Attempt:
-//QUESTION: does a custom element need a class or can we simply define it as a function?
+//My Attempt
 class trialComponent extends HTMLElement {
     constructor(tagName, strings, args){
         super();
@@ -31,9 +28,14 @@ class trialComponent extends HTMLElement {
         }
     }
 }
-customElements.define('user-section',User)
+customElements.define('user-section', User)
 //COULDI: day5 define an array of what I am already going to add in the innerhtml of the webcomponent and then render it on the connected call back? is there a way to not need to do that?
-//COULDI: day5 create a way of making a customcomponent that does what mfrachet does and then build out my boiler plate components as child classes from there?
+//COULDI: day5 create a way of making a custom component that does what mfrachet does and then build out my boiler plate components as child classes from there?
+//COULDI: day6 use a factory pattern in order to create custom tag names within new components?
+//HOWDOI: day6 define a class that I don't know the name of yet?
 //HOWDOI: day5 make a curried function into a method in js? perhaps https://stackoverflow.com/questions/42898930/create-es6-class-methods-from-other-class-methods
 //HOWDOI: day5 make a class constructor with an ...args method?
 //QUESTION: maybe I'm doing this the wrong way around? Perhaps I define the tagname at the mounter rather than within the component class itself?? I think the way I'm going about it here I'm going to have to mount the curried function to the class component and then mount the class component to the DOM, its going to take up too much time
+//QUESTION: day5 why does he use {} with the ({}) in the USER arguments?
+//CONCEPT: day5 What this approach is missing is being able to use the custom tag name within the element
+//QUESTION: does a custom element need a class or can we simply define it as a function? - Check the component mounter file in order to see where I've thought of this more

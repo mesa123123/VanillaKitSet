5f1036f033f5976ba78d37fc4c375f53c3f7c1e0
mesa123123 <bowmanpete123@gmail.com>
2019-11-20 07:41:00 +1300

    day6, pt2

--- src/javascript/componentMounter.js
+++ src/javascript/componentMounter.js
@@ -13,4 +13,4 @@ export default mountComponent
 //CONCEPT: Vanilla Router Day 3 The create frontend framework tutorial shows a new element being created and then new text node being created, all out of the web component itself (the web component having both a type and a template and instead of passing in the mainDiv the name of the mainDiv is given and then queried from the page instead)
 //COULDI: day6 because the customElements.define method takes a constructor it could take a custom method that outputs a class method?
 //HOWDOI: day6 if I am to create custom components at the mounter in order to have custom named tags at the DOM I've got to somehow create new HTMLElement objects off of the templates in the component classes I've made in the component files
-//HOWDOI: day5 Maybe I just use a function in the customelements.define here???
+//HOWDOI: day5 Maybe I just use a function in the customelements.define here??? -> Check this: https://stackoverflow.com/questions/33605775/es6-dynamic-class-names
